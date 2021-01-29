0d534b2021f611083c30891836f86266f0ed965c
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2021-01-29 17:40:42 +0530

    Login enj page set up

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const asyncHandler = require("../../lib/core/asyncHandler")
 const UserRepo = require("../../database/mongoose/repositories/UserRepo")
+const ApiKeyRepo = require("../../database/mongoose/repositories/ApiKeyRepo")
 const { BadRequestError } = require("../../lib/core/apiError")
+const { SuccessResponse } = require("../../lib/core/apiResponse")
 const { frontEndLogin } = require("../../config")
 const router = express.Router()
 
@@ -11,6 +13,27 @@ router.get("/resetPassword/:token/:apiKey", asyncHandler(async (req, res) => {
   if (user === null) throw BadRequestError("Invalid or expired token")
   return res.render("resetPassword", {
     email: user.email, token, apiKey, frontEndLogin
+  })
+}))
+
+
+router.get("/login", asyncHandler(async (req, res) => {
+  const {
+    scope,
+    responseType,
+    clientId: apiKey,
+    redirectUri,
+    state,
+  } = req.query
+  console.log("apiKey", apiKey)
+
+  const client = await ApiKeyRepo.findByKey(apiKey)
+  // validation
+  if (client === null) throw BadRequestError("clientId is not registered !!!")
+  if (client.redirectUri !== redirectUri) throw BadRequestError("redirectUri is not registered !!!")
+
+  return res.render("login", {
+    apiKey, frontEndLogin
   })
 }))
 
